c3068af80fb5439f8fed2248e60d372466857409
npoilvet <nat.poilvet@gmail.com>
2018-09-27 09:32:54 +0000

    1ere partie TP2

--- server.js
+++ server.js
@@ -5,17 +5,28 @@ const app = express();
 const port = 3000;
 
 
+const os = require("os");
+const mustacheExpress = require('mustache-express');
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+app.use(express.static('public'))
 app.get('/', (request, response) => {
-    response.send('Requête reçue...')
-});
+  response.render('index');
+})
 
 app.get('/hello/:name', (request, response) => {
-    response.send('Hello, ' + request.params.name + '!');
-});
+  response.render('hello', {name: request.params.name});
+})
+app.get('/pooc', (request, response) => {
+  response.render('pooc');
+})
 
 app.listen(port, (err) => {
-    if (err) {
-        return console.log('Erreur du serveur : ', err)
-    }
-    console.log('Le serveur écoute sur le port', port)
-});
+  if (err) {
+    return console.log('Erreur du serveur : ', err)
+  }
+
+  console.log('Le serveur écoute sur le port '+port+'\nRendez vous sur http://'+os.hostname()+'.local:'+port);
+})
+
